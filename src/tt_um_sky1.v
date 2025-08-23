bbda55e6452024a7b92636cf3033ca8bd58bdef0
Goutham-Badhrinath <gouthambadhrinath123@gmail.com>
2025-08-24 02:14:40 +0530

    Update tt_um_sky1.v

--- src/tt_um_sky1.v
+++ src/tt_um_sky1.v
@@ -19,7 +19,7 @@ module tt_um_sky1(
     assign uio_oe = 8'h00;
     assign uio_out = 8'h00;
     assign uo_out = AC;
-    reg [7:0] instruction_mem [0:29];
+    reg [7:0] instruction_mem [0:26];
     reg [1:0] state;
     reg [7:0] opcode;
     reg [7:0] operand;
